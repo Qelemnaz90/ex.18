9571773f9f75f31ec59e0734dbe9b8f29673a0ec
Qelemnaz90 <enazy1990@gmail.com>
2024-03-06 19:50:09 +0400

    index

---
+++ index.js
@@ -0,0 +1,21 @@
+var phones = ["Samsung", "Apple", "Sony", "Acer"]
+var bands = [ "Qaya", "Beatles", "Kristal", "Abba"  ]
+var heroes = [ "Superman", "Ironman", "Alice", "Ladybug" ]
+var friends = ["Ayten", "Gulsen", "Gunel", "Gunay"]
+
+for ( var i = 0 ; i < phones.length ; i++ ) {
+console.log(phones [i]) }
+
+console.log( "          " )
+
+for ( var b=0; b < bands.length; b++ ) {console.log(bands[b])}
+
+console.log( "            " )
+
+
+for (var h = 0; h < heroes.length; h++ )   {console.log(heroes[h])}
+
+
+console.log("                         ")
+
+for (var f = 0; f < friends.length; f++ ) {console.log(friends[f])}
